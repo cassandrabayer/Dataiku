0a12bd3a8d8a30c02d4973a40d91a1bc5b1f0a86
cassandrabayer <cassie.bayer@gmaill.com>
2018-06-13 21:06:19 -0400

    light processing

--- controller.R
+++ controller.R
@@ -9,6 +9,7 @@ setwd("/Users/cassandrabayer/Desktop/Dataiku")
 library(data.table)
 library(tidyr)
 library(tidyverse)
+library(stringr)
 
 # visualization packages
 library(ggplot2)
@@ -30,16 +31,35 @@ library(lubridate)
 
 
 # Load Data ---------------------------------------------------------------
-census_train <- read.csv(file = "census_income_learn.csv", stringsAsFactors = F,header = T)
+census_train <- read.csv(file = "census_income_learn.csv", stringsAsFactors = F, header = T)
+census_test <- read.csv(file = "census_income_test.csv", stringsAsFactors = F, header = T)
 census_train <- data.table(census_train)
+census_test <- data.table(census_test)
 
 ## I know I'm losing data but 1 row out of 200k will be of negligble significance in this case
 setnames(census_train, dput(names(census_train)),
-                         c("age", "classOfWorker", "industry", "adjGrossIncome", "edu", "wageHr", "eduInLastWk", "maritalStat", "majorIndustry",
-                           "occupation code", "mac", "hispanic", "sex", "laborUnion", "unemploymentReason", "employmentStatus", "capGains", "capLoss",
-                           "stocks", "fedIncTaxLiable", "taxStatus", "region", "state", "hhStat", "instanceWt", "migrationMSA", "migrationReg",
-                           "migrationWithInReg", "house1PlusYr", "prevResInSunbelt", "pplWorkForEmp", "fam18under", "totalEarn", "fatherOrigin",
-                           "motherOrigin", "selfOrigin", "citizenship", "totalInc", "bizOrSelfEmp", "taxableInc", "vetAdmin", "weeksWorkedPastYr"))
+         c("age", "classOfWorker", "industry", "adjGrossIncome", "edu", "wageHr", "eduInLastWk", "maritalStat", "majorIndustry",
+           "occupation code", "race", "hispanic", "sex", "laborUnion", "unemploymentReason", "employmentStatus", "capGains", "capLoss",
+           "stocks", "fedIncTaxLiable", "taxStatus", "region", "state", "hhStat", "instanceWt", "migrationMSA", "migrationReg",
+           "migrationWithInReg", "house1PlusYr", "prevResInSunbelt", "pplWorkForEmp", "fam18under", "fatherOrigin",
+           "motherOrigin", "selfOrigin", "citizenship", "bizOrSelfEmp", "vetAdmin", "vetBens", "weeksWorkedPastYr", "year", "over50k"))
 
-census_test <- read.csv()
+setnames(census_test, dput(names(census_test)),
+         c("age", "classOfWorker", "industry", "adjGrossIncome", "edu", "wageHr", "eduInLastWk", "maritalStat", "majorIndustry",
+           "occupation code", "race", "hispanic", "sex", "laborUnion", "unemploymentReason", "employmentStatus", "capGains", "capLoss",
+           "stocks", "fedIncTaxLiable", "taxStatus", "region", "state", "hhStat", "instanceWt", "migrationMSA", "migrationReg",
+           "migrationWithInReg", "house1PlusYr", "prevResInSunbelt", "pplWorkForEmp", "fam18under", "fatherOrigin",
+           "motherOrigin", "selfOrigin", "citizenship", "bizOrSelfEmp", "vetAdmin", "vetBens", "weeksWorkedPastYr", "year", "over50k"))
 
+
+# Light Processing --------------------------------------------------------
+census_train[grepl(x = over50k, pattern = "-"), over50k := "0"]
+census_train[grepl(x = over50k, pattern = "+"), over50k := "1"]
+
+census_test[grepl(x = over50k, pattern = "-"), over50k := "0"]
+census_test[grepl(x = over50k, pattern = "+"), over50k := "1"]
+
+census_train[, .SD := str_replace(.SD, "Not in universe", "NA"), .SDcols = names(census_train)]
+sapply(census_test, str_replace)
+
+class(cent)
