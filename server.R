f7358b104c81faf3bd3e854e5ac21792e1979969
cassandrabayer <cassie.bayer@gmaill.com>
2018-06-15 23:37:01 -0400

    finalized and published

--- server.R
+++ server.R
@@ -1,7 +1,8 @@
 
 shinyServer(function(input, output) {
   
-  lm1 <- reactive({lm(reformulate(input$IndVar, input$DepVar), data = census_train, family = binomial(link = "logit"))})
+  lm1 <- reactive({lm(reformulate(input$IndVar, input$DepVar), data = census_train$dtClean, 
+                      family = binomial(link = "logit"))})
   
   output$DepPrint <- renderPrint({input$DepVar})
   output$IndPrint <- renderPrint({input$IndVar})
--- ui.R
+++ ui.R
@@ -1,12 +1,12 @@
 
 
 shinyUI(fluidPage(
-  headerPanel("Regression Testing"), 
+  headerPanel("Dataiku: Preliminary Regression Testing"), 
   sidebarPanel(
     p("Select the inputs for the Independent Variable"),
     selectInput(inputId = "IndVar", label = "Independent Variables", 
                 multiple = F, 
-                choices = names(census_train[, 1:31])),
+                choices = names(census_train$dtClean[, 1:31])),
     p("Select the inputs for the Dependent Variable"),
     selectInput(inputId = "DepVar", label = "Dependent Variables", multiple = FALSE, choices = list( "over50k"))
   ),
